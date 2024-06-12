5f330aef914e0c7074b5aae10a215e67813716ac
vkkd-dev <vargabdas@gmail.com>
2024-06-12 16:46:38 +0530

    fixed admin dashboard for mobile view

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -74,8 +74,8 @@ const AdminPage = () => {
 
       {!isAdminLoggedIn && !isLoading && (
         <>
-          <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-            <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+          <div className="container relative flex h-auto min-h-screen flex-col items-center justify-center lg:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+            <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
               <div className="absolute inset-0 bg-zinc-900" />
               <div className="relative z-20 flex items-center text-lg font-medium">
                 Northeast Voyage
@@ -88,14 +88,13 @@ const AdminPage = () => {
                   alt="Authentication"
                 />
               </div>
-              <div className=" relative z-20 mt-auto">
+              <div className="relative z-20 mt-auto">
                 <blockquote className="space-y-2">
                   <p className="text-lg text-center">Welcome back, Admin</p>
-                  {/* <footer className="text-sm text-center">Welcome back, Admin</footer> */}
                 </blockquote>
               </div>
             </div>
-            <div className="lg:p-8">
+            <div className="p-6 lg:p-8 w-full">
               <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
                 <div className="flex flex-col space-y-2 text-center">
                   <h1 className="text-2xl font-semibold tracking-tight">
@@ -107,22 +106,22 @@ const AdminPage = () => {
                 </div>
                 <UserAuthForm />
                 {/* <p className="px-8 text-center text-sm text-muted-foreground">
-                  By clicking continue, you agree to our{" "}
-                  <Link
-                    href="/terms"
-                    className="underline underline-offset-4 hover:text-primary"
-                  >
-                    Terms of Service
-                  </Link>{" "}
-                  and{" "}
-                  <Link
-                    href="/privacy"
-                    className="underline underline-offset-4 hover:text-primary"
-                  >
-                    Privacy Policy
-                  </Link>
-                  .
-                </p> */}
+                By clicking continue, you agree to our{" "}
+                <Link
+                  href="/terms"
+                  className="underline underline-offset-4 hover:text-primary"
+                >
+                  Terms of Service
+                </Link>{" "}
+                and{" "}
+                <Link
+                  href="/privacy"
+                  className="underline underline-offset-4 hover:text-primary"
+                >
+                  Privacy Policy
+                </Link>
+                .
+              </p> */}
               </div>
             </div>
           </div>
